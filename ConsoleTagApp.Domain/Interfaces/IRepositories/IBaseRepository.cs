f48da9d0666a4b2ef33b71e78211360c34c84dfa
OlegSvekla <olegsvekla993@gmail.com>
2023-11-05 14:04:44 +0300

    add services conf to program, repositories

---
+++ ConsoleTagApp.Domain/Interfaces/IRepositories/IBaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleTagApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTagApp.Domain.Interfaces.IRepositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllByAsync(Func<IQueryable<T>,
+            IIncludableQueryable<T, object>>? include = null,
+            Expression<Func<T, bool>>? expression = null,
+            CancellationToken cancellationToken = default);
+
+        Task<T> GetOneByAsync(Func<IQueryable<T>,
+            IIncludableQueryable<T, object>>? include = null,
+            Expression<Func<T, bool>>? expression = null,
+            CancellationToken cancellationToken = default);
+
+        Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    }
+}
