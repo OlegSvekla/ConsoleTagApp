71de27944b1b37c0e88c9444cb9f37ed984f9656
OlegSvekla <olegsvekla993@gmail.com>
2023-11-05 13:33:27 +0300

    add entities, baseentities and configurations for entities

---
+++ ConsoleTagApp.Domain/Entities/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTagApp.Domain.Entities
+{
+    public class Tag : BaseEntity
+    {
+        [Required]
+        public string Value { get; set; } = default!;
+
+        [Required]
+        public string Domain { get; set; } = default!;
+
+        public List<User>? Users { get; set; }
+    }
+}
---
+++ ConsoleTagApp.Infrastructure/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleTagApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTagApp.Infrastructure.EntitiesConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Name).IsRequired();
+            builder.Property(u => u.Domain).IsRequired();
+
+            builder.HasMany(u => u.TagsToUser)
+                .WithOne(tu => tu.User)
+                .HasForeignKey(tu => tu.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
